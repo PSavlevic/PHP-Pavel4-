5885072cf681d7c882236ac2a35f1f703293ada9
Pavel <p.savlevic@gmail.com>
2019-05-24 10:52:14 +0300

    test

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="1">
+    <title>title</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+</body>
+</html>
