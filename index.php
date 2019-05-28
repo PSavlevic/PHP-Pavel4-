81b4f4856fbf62cac5f238eaf99557690e69a2fc
Pavel <p.savlevic@gmail.com>
2019-05-28 09:29:38 +0300

    Kebabines (uzsakymo forma)

--- index.php
+++ index.php
@@ -1,46 +1,107 @@
 <?php
 
-$vardas = '';
-$pareigos = '';
-$url = '';
-$area = '';
+$pasiula = [
+    'kebabines' => [
+        'Jammi',
+        'Wraperia Suppa Kebs',
+        'Sinano kebabai',
+    ],
+    'tipas' => [
+        'kebabas_lavase',
+        'kebabas_pitoje',
+        'kebabas_leksteje',
+    ],
+    'dydis' => [
+        'didelis',
+        'mazas',
+        'kovotojo',
+    ],
+    'padazas' => [
+        'cesnakinis',
+        'svelnus',
+        'cipotle',
+        'astrus',
+    ],
+    'mesa' => [
+        'jautiena',
+        'vistiena',
+    ],
+];
 
-if (isset($_POST['send'])) {
-    $vardas = $_POST['vardas'];
-    $pareigos = $_POST['pareigos'];
-    $url = $_POST['url'];
-    $area = $_POST['area'];
+if (!empty($_POST['kebabines']) && ($_POST['tipas']) && ($_POST['dydis']) && ($_POST['padazas']) && ($_POST['mesa'])) {
+    $kebabines = $pasiula['kebabines'][$_POST['kebabines']];
+    $tipas = $pasiula['tipas'][$_POST['tipas']];
+    $dydis = $pasiula['dydis'][$_POST['dydis']];
+    $padazas = $pasiula['padazas'][$_POST['padazas']];
+    $mesa = $pasiula['mesa'][$_POST['mesa']];
+} else {
+    $kebabines = 'nepasirinkta';
+    $tipas = 'nepasirinkta';
+    $dydis = 'nepasirinkta';
+    $padazas = 'nepasirinkta';
+    $mesa = 'nepasirinkta';
 }
 
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>title</title>
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<main>
-    <section class="input-div">
-        <h1>Anketa:</h1>
-        <form method="post">
-            <input type="text" name="vardas" placeholder="Vardas">
-            <input type="text" name="pareigos" placeholder="Pareigos">
-            <input type="url" name="url" placeholder="https://example.com" pattern="https://.*">
-            <textarea name="area" id="area" placeholder="Aprasymas..."></textarea>
-            <input type="submit" value="Siusk" name="send">
-        </form>
-    </section>
-    <section class="output-div">
-        <h1>Profilis:</h1>
-            <img class="transition-rotate" src="<?php print $url; ?>" alt="img">
-        <h2>Vardas: <?php print $vardas; ?> </h2>
-        <h2>Pareigos: <?php print $pareigos; ?> </h2>
-        <h2>Aprasymas: <?php print $area; ?> </h2>
-    </section>
-</main>
-
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Kebabai</title>
+        <link rel="stylesheet" type="text/css" href="css/normalise.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    <body>
+        <section>
+            <form method="post">
+                <?php foreach ($pasiula as $key => $pasirinkimai): ?>
+                    <select name="<?php print $key; ?>">
+                        <?php foreach ($pasirinkimai as $key => $kebabine): ?>
+                            <option value="<?php print $key; ?>">
+                                <?php print $kebabine; ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                <?php endforeach; ?>
+                <input type="submit" value="uzsakyti">
+            </form>
+            <table>
+                <tr>
+                    <th>
+                        Kebabine:
+                    </th>
+                    <th>
+                        Kebabo tipas:
+                    </th>
+                    <th>
+                        Kebabo dydis:
+                    </th>
+                    <th>
+                        Kebabo padazas:
+                    </th>
+                    <th>
+                        Mesa:
+                    </th>
+                </tr>
+                <tr>
+                    <td>
+                        <?php print $kebabines; ?>
+                    </td>
+                    <td>
+                        <?php print $tipas; ?>
+                    </td>
+                    <td>
+                        <?php print $dydis; ?>
+                    </td>
+                    <td>
+                        <?php print $padazas; ?>
+                    </td>
+                    <td>
+                        <?php print $mesa; ?>
+                    </td>
+                </tr>
+            </table>
+        </section>
+    </body>
 </html>
